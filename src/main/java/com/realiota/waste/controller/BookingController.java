53fb4d3c7c04a6bc4fbc93ab34dfca9e5c4797d0
Shubham Jain <shubham.jain@rivigo.com>
2018-09-30 03:41:07 +0530

    changes

--- src/main/java/com/realiota/waste/controller/BookingController.java
+++ src/main/java/com/realiota/waste/controller/BookingController.java
@@ -2,6 +2,8 @@ package com.realiota.waste.controller;
 
 import com.realiota.waste.dto.BookingDTO;
 import com.realiota.waste.dto.Response;
+import com.realiota.waste.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/booking")
 public class BookingController {
 
+    @Autowired
+    private BookingService bookingService;
+
     @RequestMapping(value = "/initiate", method = RequestMethod.POST)
     public Response<BookingDTO> initiateBooking(@RequestBody BookingDTO bookingDTO) {
-        return null;
+        return new Response<>(bookingService.initiateBooking(bookingDTO));
     }
 
-    @RequestMapping(value = "/complete", method = RequestMethod.GET)
+    @RequestMapping(value = "/complete", method = RequestMethod.POST)
     public Response<BookingDTO> completeBooking(@RequestBody BookingDTO bookingDTO) {
-        return null;
+        return new Response<>(bookingService.completeBooking(bookingDTO));
     }
 }
